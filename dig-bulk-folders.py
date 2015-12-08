ab1c2c51d6db9f8d196ab309eb281c3d4595d777
Amandeep Singh <amandeep.s.saggu@gmail.com>
2015-12-08 18:58:38 +0530

    'class ify dig-bulk-folders

--- dig-bulk-folders.py
+++ dig-bulk-folders.py
@@ -1,242 +1,197 @@
 import json
 from http.client import HTTPSConnection
 from base64 import b64encode
+import ConfigParser
+from elasticsearch_manager import ElasticSearchManager
 import codecs
 
-feature_names = [
-    'person_ethnicity_feature',
-    'phonenumber_feature',
-    'place_postalAddress_feature',
-    'provider_name_feature',
-    'person_age_feature'
-]
+class BulkFolders(object):
 
+    def __init__(self):
+        self.feature_names = [
+                    'person_ethnicity_feature',
+                    'phonenumber_feature',
+                    'place_postalAddress_feature',
+                    'provider_name_feature',
+                    'person_age_feature'
+                ]
 
-def get_feature_values(obj_or_array):
-    """Given the values of a single feature, return them as a string.
-    If the feature has a single calue we get an object. If it has
-    multiple values we get an array and return the values separated with |."""
-    # Possible fix needed here to convert the result to unicode instead of
-    # using str()
-    result = ''
-    if isinstance(obj_or_array, list):
-        result = "|".join(map(lambda x: str(x['featureValue']), obj_or_array))
-    else:
-        result = str(obj_or_array['featureValue'])
-    if result is None:
+        self.ht_headings = [
+                    'hasIdentifier',
+                    'dateCrawled',
+                    'dateCreated',
+                    'dateModified',
+                    'url',
+                    'hasTitlePart',
+                    'hasBodyPart'
+                ]
+        self.ht_headings.extend(self.feature_names)
+
+        configuration=ConfigParser.RawConfigParser()
+        configuration.read('config.properties')
+        self.bulkfoldersurl=configuration.get('BulkFolders','bulkapiurl')
+        self.bulkfoldersuser=configuration.get('BulkFolders','bulkapiuser')
+        self.bulkfolderspassword=configuration.get('BulkFolders','bulkapipassword')
+
+    def get_feature_values(self,obj_or_array):
+        """Given the values of a single feature, return them as a string.
+        If the feature has a single calue we get an object. If it has
+        multiple values we get an array and return the values separated with |."""
+        # Possible fix needed here to convert the result to unicode instead of
+        # using str()
         result = ''
-
-    return result
-
-
-def get_feature_collection_values(fc):
-    """Get the values of all features in feature collection fc."""
-    result = []
-    for x in feature_names:
-        if (x in fc):
-            result.append(get_feature_values(fc[x]))
+        if isinstance(obj_or_array, list):
+            result = "|".join(map(lambda x: str(x['featureValue']), obj_or_array))
         else:
-            result.append('')
-    return result
+            result = str(obj_or_array['featureValue'])
+        if result is None:
+            result = ''
+
+        return result
 
 
-ht_headings = [
-    'hasIdentifier',
-    'dateCrawled',
-    'dateCreated',
-    'dateModified',
-    'url',
-    'hasTitlePart',
-    'hasBodyPart'
-]
-ht_headings.extend(feature_names)
+    def get_feature_collection_values(self,fc):
+        """Get the values of all features in feature collection fc."""
+        result = []
+        for x in self.feature_names:
+            if (x in fc):
+                result.append(self.get_feature_values(fc[x]))
+            else:
+                result.append('')
+        return result
+
+    def stringify_value(self,value):
+        "If value is a list, reduce to a string."
+        if isinstance(value, list):
+            return "|".join(value)
+        else:
+            return value
 
 
-def stringify_value(value):
-    "If value is a list, reduce to a string."
-    if isinstance(value, list):
-        return "|".join(value)
-    else:
-        return value
+    def ht_to_array(self,ht):
+        """Create an array containing all the values we want to export from a
+        WebPage object, ht."""
+        identifier = ht.get('hasIdentifier', {})
+        identifier_str = ''
+        if isinstance(identifier, list):
+            identifier_str = "|".join(
+                map(lambda x: x.get('label', ''), identifier_str))
+        else:
+            identifier_str = identifier.get('label', '')
+
+        body_text = ht.get('hasBodyPart', {}).get('text', '')
+        # http://stackoverflow.com/questions/3224268/python-unicode-encode-error
+        if type(body_text) == str:
+            body = unicode(body_text, "utf-8", errors="ignore")
+        else:
+            body = unicode(body_text)
+        body = body.replace('\n', ' ').replace('\t', ' ').replace('\r', ' ')
+        # I give up, the exported TSV file looks correct, Karma can import it
+        # correctly, but it is messed up in Excel or Numbers
+        body = 'removed'
+
+        result = [
+            identifier_str,
+            ht.get('dateCrawled', ''),
+            ht.get('dateCreated', ''),
+            ht.get('dateModified', ''),
+            ht.get('url', ''),
+            self.stringify_value(ht.get('hasTitlePart', {}).get('text', '')),
+            body
+        ]
+        result.extend(self.get_feature_collection_values(ht['hasFeatureCollection']))
+        return result
+
+    def get_ads(self,uri_list):
+        """Get a set of ads from an ES index.
+        eg "els.istresearch.com:39200"
+        """
+        esm = ElasticSearchManager()
+        results=esm.search_es(esm.create_ids_query(uri_list))
+        # print(results)
+        hits = results['hits']['hits']
+        return map(lambda x: x['_source'], hits)
 
 
-def ht_to_array(ht):
-    """Create an array containing all the values we want to export from a
-    WebPage object, ht."""
-    identifier = ht.get('hasIdentifier', {})
-    identifier_str = ''
-    if isinstance(identifier, list):
-        identifier_str = "|".join(
-            map(lambda x: x.get('label', ''), identifier_str))
-    else:
-        identifier_str = identifier.get('label', '')
+    def add_folder_to_uri_to_folder_map(self,folder, dictionary):
+        """Add a folder to the uri_to_folder map."""
+        name = folder['name']
+        for item in folder['FolderItems']:
+            uri = item['elasticId']
+            entry = dictionary.get(uri, [])
+            entry.append(name)
+            dictionary[uri] = entry
 
-    body_text = ht.get('hasBodyPart', {}).get('text', '')
-    # http://stackoverflow.com/questions/3224268/python-unicode-encode-error
-    if type(body_text) == str:
-        body = unicode(body_text, "utf-8", errors="ignore")
-    else:
-        body = unicode(body_text)
-    body = body.replace('\n', ' ').replace('\t', ' ').replace('\r', ' ')
-    # I give up, the exported TSV file looks correct, Karma can import it
-    # correctly, but it is messed up in Excel or Numbers
-    body = 'removed'
+    def construct_uri_to_folder_map(self,folder_list):
+        """Convert a list of folders into a dictionary {uri: [folders]}"""
+        uri_to_folders_map = {}
+        for folder in folder_list:
+            self.add_folder_to_uri_to_folder_map(folder, uri_to_folders_map)
+        return uri_to_folders_map
 
-    result = [
-        identifier_str,
-        ht.get('dateCrawled', ''),
-        ht.get('dateCreated', ''),
-        ht.get('dateModified', ''),
-        ht.get('url', ''),
-        stringify_value(ht.get('hasTitlePart', {}).get('text', '')),
-        body
-    ]
-    result.extend(get_feature_collection_values(ht['hasFeatureCollection']))
-    return result
+    def dereference_uris(self,dictionary):
+        """"""
+        result = []
+        ads = self.get_ads(dictionary.keys())
+        for ad in ads:
+            folders_names = dictionary[ad['uri']]
+            tab_separated = '|'.join(folders_names) + '\t' + "\t".join(self.ht_to_array(ad))
 
+            result.append(tab_separated)
 
-def get_ad(credentials, connection_url, uri):
-    """Get an ad from an ES index.
-    eg "els.istresearch.com:39200"
-    """
+        return result
 
-    query = {
-        "filter": {
-            "term": {}
+    def get_folders(self,username):
+        """Return JSON array of folder contents"""
+
+        credentials = self.bulkfoldersuser + ":" + self.bulkfolderspassword
+        conn = HTTPSConnection(self.bulkfoldersurl)
+        userAndPass = b64encode(credentials).decode("ascii")
+        headers = {
+            'cache-control': "no-cache",
+            'Authorization': 'Basic %s' % userAndPass
         }
-    }
-    query['filter']['term']['uri'] = uri
 
-    conn = HTTPSConnection("dig.istresearch.com:5443")
-    userAndPass = b64encode(credentials).decode("ascii")
-    headers = {
-        'cache-control': "no-cache",
-        'Authorization': 'Basic %s' % userAndPass
-    }
+        conn.request("GET", "/api/users/" + username + "/folders", headers=headers)
 
-    conn.request(
-        "POST",
-        "/dig-latest/WebPage/_search",
-        json.dumps(query),
-        headers
-    )
-    res = conn.getresponse()
-    data = res.read()
+        res = conn.getresponse()
+        data = res.read()
 
-    object = json.loads(data.decode("utf-8"))
-    return object['hits']['hits'][0]['_source']
+        return json.loads(data.decode("utf-8"))
 
+    def format_tsv_lines(self,lines):
+        """Write the lines to standard output.
+        This does not work because of the following error:
+          UnicodeEncodeError:
+          'ascii' codec can't encode characters in position 150-151:
+          ordinal not in range(128)
+        Apparently the problem is that the console cannot display it,
+        but it is OK to put in a file.
+        The write_tsv file will work correctly.
+        """
+        print('folder\t' + '\t'.join(self.ht_headings))
+        for line in lines:
+            print(line)
 
-def get_ads(credentials, connection_url, uri_list):
-    """Get a set of ads from an ES index.
-    eg "els.istresearch.com:39200"
-    """
-    query = {
-        "filter": {
-            "terms": {}
-        }
-    }
-    query['size'] = len(uri_list)
-    query['filter']['terms']['uri'] = uri_list
-    conn = HTTPSConnection("dig.istresearch.com:5443")
-    userAndPass = b64encode(credentials).decode("ascii")
-    headers = {
-        'cache-control': "no-cache",
-        'Authorization': 'Basic %s' % userAndPass
-    }
+    def construct_tsv_response(self,lines):
+        self.write_tsv('folder_export.tsv',lines)
+        tsv_response='folder\t' + '\t'.join(self.ht_headings)+'\n'
+        tsv_response = tsv_response + '\n'.join(lines)
+        return tsv_response
 
-    conn.request(
-        "POST",
-        "/dig-latest/WebPage/_search",
-        json.dumps(query),
-        headers
-    )
-    res = conn.getresponse()
-    data = res.read()
-
-    results = json.loads(data.decode("utf-8"))
-    # print(results)
-    hits = results['hits']['hits']
-    return map(lambda x: x['_source'], hits)
+    def write_tsv(self,file_name, lines):
+        "Write the lines for each ad to a file."
+        file = codecs.open(file_name, "w", "utf-8")
+        file.write('folder\t' + '\t'.join(self.ht_headings)+'\n')
+        file.write('\n'.join(lines))
 
 
-def add_folder_to_uri_to_folder_map(folder, dictionary):
-    """Add a folder to the uri_to_folder map."""
-    name = folder['name']
-    for item in folder['FolderItems']:
-        uri = item['elasticId']
-        entry = dictionary.get(uri, [])
-        entry.append(name)
-        dictionary[uri] = entry
-
-
-def construct_uri_to_folder_map(folder_list):
-    """Convert a list of folders into a dictionary {uri: [folders]}"""
-    uri_to_folders_map = {}
-    for folder in folder_list:
-        add_folder_to_uri_to_folder_map(folder, uri_to_folders_map)
-    return uri_to_folders_map
-
-
-def dereference_uris(credentials, connection_url, dictionary):
-    """"""
-    result = []
-    ads = get_ads(credentials, connection_url, dictionary.keys())
-    for ad in ads:
-        folders_names = dictionary[ad['uri']]
-        tab_separated = '|'.join(folders_names) + '\t' + "\t".join(ht_to_array(ad))
-
-        result.append(tab_separated)
-
-    return result
-
-
-def get_folders(credentials, host):
-    """Return JSON array of folder contents"""
-
-    conn = HTTPSConnection(host)
-    userAndPass = b64encode(credentials).decode("ascii")
-    headers = {
-        'cache-control': "no-cache",
-        'Authorization': 'Basic %s' % userAndPass
-    }
-
-    conn.request("GET", "/api/users/txdps/folders", headers=headers)
-
-    res = conn.getresponse()
-    data = res.read()
-
-    return json.loads(data.decode("utf-8"))
-
-
-def format_tsv_lines(lines):
-    """Write the lines to standard output.
-    This does not work because of the following error:
-      UnicodeEncodeError:
-      'ascii' codec can't encode characters in position 150-151:
-      ordinal not in range(128)
-    Apparently the problem is that the console cannot display it,
-    but it is OK to put in a file.
-    The write_tsv file will work correctly.
-    """
-    print('folder\t' + '\t'.join(ht_headings))
-    for line in lines:
-        print(line)
-
-
-def write_tsv(file_name, lines):
-    "Write the lines for each ad to a file."
-    file = codecs.open(file_name, "w", "utf-8")
-    file.write('folder\t' + '\t'.join(ht_headings)+'\n')
-    file.write('\n'.join(lines))
-
-
-write_tsv(
-    'folder_export.tsv',
-    dereference_uris(
-        'login:password!',
-        'dig.istresearch.com:5443',
-        construct_uri_to_folder_map(get_folders(
-            'login:password',
-            'dig.istresearch.com:5443'))))
+#write_tsv(
+#    'folder_export.tsv',
+#    dereference_uris(
+#        'login:password!',
+#        'dig.istresearch.com:5443',
+#        construct_uri_to_folder_map(get_folders(
+#            'login:password',
+#            'dig.istresearch.com:5443'))))
 
--- process_request.py
+++ process_request.py
@@ -5,6 +5,7 @@ import sys
 from flask import request
 import json
 from elasticsearch_manager import ElasticSearchManager
+from dig-bulk-folders import BulkFolders
 
 app = Flask(__name__)
 
@@ -14,22 +15,35 @@ def processcsv():
         json_data = json.loads(str(request.get_data()))
         esm=ElasticSearchManager()
 
+        bf = BulkFolders()
+
         es_request = esm.convert_csv_to_esrequest(json_data['csv'])
 
         if 'ids' in es_request:
-            process_results(esm.search_es(esm.create_ids_query(es_request['ids'])))
+            return process_results(esm.search_es(esm.create_ids_query(es_request['ids'])))
         if 'phone' in es_request:
-            process_results(esm.search_es(esm.create_terms_query('phone',es_request['phone'])))
-
+            return process_results(esm.search_es(esm.create_terms_query('phone',es_request['phone'])))
 
     except Exception as e:
         print >> sys.stderr,e
         loge(str(e))
 
+@app.route('/export/<username>',method=['POST'])
+def get_user_folders(username):
+    bf = BulkFolders()
+    return bf.construct_tsv_response(bf.dereference_uris(bf.construct_uri_to_folder_map(bf.get_folders(username))))
 
 
-def process_results(res):
-    return res
+def process_results(bf,res):
+    result=[]
+
+    hits = res['hits']['hits']
+    ads = map(lambda x: x['_source'], hits)
+    for ad in ads:
+        tab_separated = "\t".join(bf.ht_to_array(ad))
+        result.append(tab_separated)
+    return result
+
 
 def loge(message):
 
